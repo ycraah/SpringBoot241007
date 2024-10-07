de76d241aa535d8570888d9854f3d22ae92314d7
김예찬 <ycraah@gamil.com>
2024-10-07 20:42:35 +0900

    "타임리프와 JSON으로 화면 출력하기 테스트"

---
+++ src/main/java/com/ycraah/springboot241007/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.ycraah.springboot241007.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class SampleController {
+  @GetMapping("/hello")
+  public String hello(Model model) {
+    log.info("hello");
+    model.addAttribute("msg", "Hello World");
+    return "hello";
+  }
+}
