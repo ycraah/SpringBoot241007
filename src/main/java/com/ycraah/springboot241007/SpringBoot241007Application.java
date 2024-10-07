0d9549b602b9af4db286aa130bc772dc88a1fb20
김예찬 <ycraah@gamil.com>
2024-10-07 15:52:33 +0900

    세팅

---
+++ src/main/java/com/ycraah/springboot241007/SpringBoot241007Application.java
@@ -0,0 +1,13 @@
+package com.ycraah.springboot241007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot241007Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot241007Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/ycraah/springboot241007/SpringBoot241007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycraah.springboot241007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot241007ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
